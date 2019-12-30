a680ae60fa6b180a639ea04714ee9d2912d65f86
taylor <pangjian78@gmail.com>
2019-12-31 00:17:07 +0800

    first commit

---
+++ ffmpeg-GUI/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ffmpeg_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cut = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.change = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // cut
+            // 
+            resources.ApplyResources(this.cut, "cut");
+            this.cut.Name = "cut";
+            this.cut.UseVisualStyleBackColor = true;
+            this.cut.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            // 
+            // textBox4
+            // 
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            // 
+            // change
+            // 
+            resources.ApplyResources(this.change, "change");
+            this.change.Name = "change";
+            this.change.UseVisualStyleBackColor = true;
+            this.change.Click += new System.EventHandler(this.change_Click);
+            // 
+            // linkLabel1
+            // 
+            resources.ApplyResources(this.linkLabel1, "linkLabel1");
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.change);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cut);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button cut;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button change;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ffmpeg-GUI/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace ffmpeg_GUI
+{
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        RunCmd runcmd = new RunCmd();
+        private static string path = @"./";
+        DirectoryInfo folder = new DirectoryInfo(path);
+        static bool flag = false;
+        static bool flagOrigin = false;
+        static bool flagExport = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+           
+            foreach (FileInfo file in folder.GetFiles("*.exe"))
+            {
+                if (file.Name.Equals("ffmpeg.exe"))
+                {
+                    flag = true;
+                }
+                Console.WriteLine(file.Name);
+            }
+            if (flag)
+            {
+                MessageBox.Show("请将本程序（仅ffmpeg-GUI.exe单个程序）拷贝至需要编辑的媒体文件目录中！\n请确保 C:\\Windows\\System32 下有 ffmpeg.exe 文件！\n否则请将压缩包内的 ffmpeg.exe 移动至C:\\Windows\\System32");
+            }
+     
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+              
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label6.Text = "状态：执行中...";
+            flagOrigin = false;
+            flagExport = false;
+            string start = textBox1.Text;
+            string end = textBox2.Text;
+            string origin = textBox3.Text;
+            string export = textBox4.Text;
+            string commond;
+            commond = "ffmpeg -ss " + start + " -to " + end + " -accurate_seek -i " + origin + " -codec copy -avoid_negative_ts 1 " + export;
+            string output;
+         
+            foreach (FileInfo file in folder.GetFiles("*.*"))
+            {
+                if (file.Name.Equals(origin))
+                {
+                    flagOrigin = true;
+                }
+                if (file.Name.Equals(export))
+                {
+                    flagExport = true;
+                }
+                Console.WriteLine(file.Name);
+            }
+
+            if (flagOrigin){
+                if (!flagExport){
+                
+                runcmd.run(commond, out output);
+                MessageBox.Show("操作完成！");
+                
+                }else{
+                    MessageBox.Show("目标文件已存在\n拒绝操作！");
+                   
+                }
+            }else{
+                MessageBox.Show("未找到媒体文件:" + origin + "\n请检查文件名及后缀是否正确");
+            }
+            label6.Text = "状态：空闲";
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("iexplore.exe", "http://blog.xn--i8s168f.cn/archives/441/");
+        }
+
+        private void change_Click(object sender, EventArgs e)
+        {
+            string end = textBox2.Text;
+            textBox1.Text = end;
+            textBox2.Text = "";
+            textBox4.Text = "";
+        }
+
+
+        }
+}
